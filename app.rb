407ca1a6965dc99ce9e981bbdccb52647e51f8c5
Dan Kiss <flowfast47@gmail.com>
2017-09-15 10:34:04 -0700

    add index, definitions and app.rb code

---
+++ app.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload(./lib/**/*.rb)
+require('.lib/definitions')
+require('pry')
+require('capybara')
+
+get('/') do
+  @the_words = Word.the_words
+  erb(:index)
+end
---
+++ lib/definitions.rb
@@ -0,0 +1,14 @@
+class Word
+
+  @@the_words = []
+
+  attr_reader(:my_word)
+
+  def initialize(attributes)
+    @my_word = attributes.fetch(:my_word)
+    @id = @@the_words.length + 1
+  end
+
+  def the_words
+    @@the_words
+  end
